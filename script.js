81d44217f8392ce8b03b70e762c737d435007f4a
Дмитрий Кречетников <dkrech07@gmail.com>
2019-08-09 18:36:14 +0500

    добавил пролистывание влево-вправо

--- script.js
+++ script.js
@@ -1,59 +1,27 @@
-var slideNumber = 1;
+var slideNumber = 0;
 var allSlides = document.querySelectorAll('.slide');
+var maxNumber = allSlides.length - 1;
 
 var btnNext = document.querySelector('.btn-next');
 var btnPrev = document.querySelector('.btn-prev');
-var slideNumber = 1;
-var maxSNumber = allSlides[slideNumber].length;
 
-btnNext.addEventListener('click', function() {
-  allSlides[slideNumber].classList.add('slide-top');
-  slideNumber++
-  console.log(slideNumber);
+var moveSlider = function() {
+  if (slideNumber < maxNumber) {
+    for (var i = 0; i < maxNumber; i++) {
+      allSlides[i].classList.remove('slide-top-btn-left');
+    }
+    allSlides[slideNumber].classList.add('slide-top-btn-left');
+    slideNumber++
 
-});
+    console.log('Прибавляется: ' + slideNumber);
+  } else {
+    slideNumber = 0;
+    for (var i = 0; i < maxNumber; i++) {
+      allSlides[i].classList.remove('slide-top-btn-left');
+    }
+    console.log('Убавляется: ' + slideNumber);
+  }
+}
 
-btnPrev.addEventListener('click', function() {
-  allSlides[maxSNumber].classList.add('slide-top');
-  maxSNumber--;
-
-});
-
-
-// function showSlides(n) {
-//   if (n > allSlides.length) {
-//     slideNumber = 1;
-//   }
-//   if (n < 1) {
-//     slideNumber = allSlides.length;
-//   }
-// }
-//
-// function slideNext() {
-//
-// }
-//
-// function slidePrev() {
-//
-// }
-//
-//
-// btnNext.addEventListener('click', function() {
-//   if (slideNumber < allSlides.length - 1) {
-//     var prevSlide = allSlides[slideNumber];
-//     slideNumber++;
-//     var currentSlide = allSlides[slideNumber];
-//     prevSlide.classList.remove('slide-bottom');
-//     prevSlide.classList.add('slide-top');
-//     currentSlide.classList.add('slide-bottom');
-//   } else {
-//     slideNumber = 0;
-//     prevSlide = allSlides[allSlides.length - 1];
-//     currentSlide = allSlides[slideNumber];
-//   }
-//
-//
-//   console.log(prevSlide);
-//   console.log(slideNumber);
-//   console.log(currentSlide);
-// });
+btnNext.addEventListener('click', moveSlider);
+btnPrev.addEventListener('click', moveSlider);
